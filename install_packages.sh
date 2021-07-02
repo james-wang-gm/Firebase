277c36252da3f2668165221f39ea275c66faf76b
james-wang-gm <78228892+james-wang-gm@users.noreply.github.com>
2021-07-02 15:22:04 -0400

    Add files via upload

---
+++ install_packages.sh
@@ -0,0 +1,7 @@
+sudo apt-get install python3-pip
+sudo pip3 install apache-beam[gcp]==2.27.0
+sudo pip3 install oauth2client==3.0.
+sudo pip3 install -U pip
+sudo pip3 install ipython
+sudo python3 -m pip install wheel
+sudo python3 -m pip install pandas
